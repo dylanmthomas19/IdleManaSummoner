1307f2959063ac92728c15951125719cabe19a9b
Dylan-Desktop <dylanmthomas@live.com>
2018-04-09 17:01:36 -0600

    trying to figure out affinity logic

--- assets/logic/game.js
+++ assets/logic/game.js
@@ -1,5 +1,28 @@
 $(function () {
+    function saveGame() {
+        localStorage.setItem('affinity', affinity)
+        localStorage.setItem('mana', mana)
+        localStorage.setItem('manaUp', manaUp)
+        localStorage.setItem('manaStorage', manaStorage)
+        localStorage.setItem('manaRateCost', manaRateCost)
+        localStorage.setItem('manaStorRituCost', manaStorRituCost)
+
+        localStorage.setItem('rituals', rituals)
+        localStorage.setItem('ritualUp', ritualUp)
+        localStorage.setItem('ritualStorage', ritualStorage)
+        localStorage.setItem('rituRateManaCost', rituRateManaCost)
+        localStorage.setItem('rituRateFamiCost', rituRateFamiCost)
+        localStorage.setItem('rituStorFamiCost', rituStorFamiCost)
+
+        localStorage.setItem('familiars', familiars)
+        localStorage.setItem('famiUp', famiUp)
+        localStorage.setItem('famiStorage', famiStorage)
+        localStorage.setItem('famiRateRituCost', famiRateRituCost)
+        localStorage.setItem('famiStorManaCost', famiStorManaCost)
+        localStorage.setItem('affinity', affinity)
+    }
     function clearSave() {
+        localStorage.setItem('affinity', '')
         localStorage.setItem('mana', 0)
         localStorage.setItem('manaUp', 1)
         localStorage.setItem('manaStorage', 20)
@@ -19,8 +42,68 @@ $(function () {
         localStorage.setItem('famiRateRituCost', 10)
         localStorage.setItem('famiStorManaCost', 200)
         window.location.reload()
-    }
+    };
+    function loadSave() {
+        localStorage.getItem('affinity')
+        localStorage.getItem(parseInt('mana'))
+        localStorage.getItem(parseInt('manaUp'))
+        if (manaUp > 1) {
+            $('#manaUpButton').show()
+            $('#manaUpCost').text(manaRateCost)
+        }
+        if (manaUp >= 5) {
+            $('#rituGrid').show()
+            $('#rituTotal').text(rituals)
+            $('#rituStorage').text(ritualStorage)
+        }
+        localStorage.getItem(parseInt('manaStorage'))
+        if (manaStorage >= 40) {
+            $('#famiGrid').show()
+            $('#famiTotal').text(familiars)
+            $('#famiStorage').text(famiStorage)
+        }
+        if (manaStorage >= 200) {
+            $('#famiStorButton').show()
+            $('#famiStorManaCost').text(famiStorManaCost)
+        }
+        localStorage.getItem(parseInt('manaRateCost'))
+        localStorage.getItem(parseInt('manaStorRituCost'))
 
+        localStorage.getItem(parseInt('rituals'))
+        if (rituals > 1) {
+            $('#manaStorageUp').show()
+            $('#storageCost').text(manaStorRituCost)
+        }
+        localStorage.getItem(parseInt('ritualUp'))
+        localStorage.getItem(parseInt('ritualStorage', ))
+        if (ritualStorage > 10) {
+            $('#famiUpButton').show()
+            $('#famiPer').text(famiUp)
+            $('#famiRateRituCost').text(famiRateRituCost)
+        }
+        localStorage.getItem(parseInt('rituRateManaCost'))
+        localStorage.getItem(parseInt('rituRateFamiCost'))
+        localStorage.getItem(parseInt('rituStorFamiCost'))
+
+        localStorage.getItem(parseInt('familiars'))
+        if (familiars > 0) {
+            $('#rituUpButton').show()
+            $('#rituPer').text(ritualUp)
+            $('#rituRateFamiCost').text(rituRateFamiCost)
+            $('#rituRateManaCost').text(rituRateManaCost)
+            $('#rituStorageUp').show()
+            $('#rituStorageFamiCost').text(rituStorFamiCost)
+        }
+        localStorage.getItem(parseInt('famiUp'))
+        localStorage.getItem(parseInt('famiStorage'))
+        localStorage.getItem(parseInt('famiRateRituCost'))
+        localStorage.getItem(parseInt('famiStorManaCost'))
+        idleRitual()
+    }
+    var affinity = localStorage.getItem('affinity');
+    if (affinity == null) {
+        affinity = ''
+    }
     var mana = parseInt(localStorage.getItem('mana'));
     if (mana === null) {
         mana = 0
@@ -33,6 +116,7 @@ $(function () {
     if (manaStorage === null) {
         manaStorage = 20
     }
+    $('#affinitySegment').hide()
     $('#manaUpButton').hide()
     $('#manaStorageUp').hide()
     $('#rituGrid').hide()
@@ -45,26 +129,9 @@ $(function () {
     $('#manaStorage').text(manaStorage)
     $('#manaPer').text(manaUp)
     $('#saveButton')
-        .on('click', function () {
-            localStorage.setItem('mana', mana)
-            localStorage.setItem('manaUp', manaUp)
-            localStorage.setItem('manaStorage', manaStorage)
-            localStorage.setItem('manaRateCost', manaRateCost)
-            localStorage.setItem('manaStorRituCost', manaStorRituCost)
-
-            localStorage.setItem('rituals', rituals)
-            localStorage.setItem('ritualUp', ritualUp)
-            localStorage.setItem('ritualStorage', ritualStorage)
-            localStorage.setItem('rituRateManaCost', rituRateManaCost)
-            localStorage.setItem('rituRateFamiCost', rituRateFamiCost)
-            localStorage.setItem('rituStorFamiCost', rituStorFamiCost)
-
-            localStorage.setItem('familiars', familiars)
-            localStorage.setItem('famiUp', famiUp)
-            localStorage.setItem('famiStorage', famiStorage)
-            localStorage.setItem('famiRateRituCost', famiRateRituCost)
-            localStorage.setItem('famiStorManaCost', famiStorManaCost)
-        })
+        .on('click', saveGame)
+    $("#loadSaveButton")
+        .on('click', loadSave)
     $('#clearSaveButton')
         .on('click', clearSave)
     $('#manaButton')
@@ -307,10 +374,340 @@ $(function () {
                 famiStorManaCost += 200
                 $('#famiStorManaCost').text(famiStorManaCost)
             }
+            if (famiStorage >= 20 && affinity == '') {
+                affinityQuiz()
+            }
         })
+    function affinityQuiz() {
+        var GoodNEvil = 0;
+        var LawNChaos = 0;
+        $('#affinityIntro')
+            .modal('show')
+        $('#affinityGo')
+            .on('click', function () {
+                $('#q1')
+                    .modal('show')
+            })
+        $('.q1a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'classical':
+                        LawNChaos++
+                        console.log(LawNChaos)
+                        break;
+                    case 'electronica':
+                        switch (LawNChaos) {
+                            case LawNChaos > 0:
+                                LawNChaos--
+                                break;
+                            case LawNChaos == 0:
+                                break;
+                            case LawNChaos < 0:
+                                LawNChaos++
+                                break;
+                        }
+                        console.log(LawNChaos)
+                        break;
+                    case 'punk':
+                        LawNChaos--
+                        console.log(LawNChaos)
+                        break;
+                }
+                $('#q2')
+                    .modal('show')
+            })
+        $('.q2a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'justice':
+                        GoodNEvil++
+                        console.log(GoodNEvil)
+                        break;
+                    case 'change':
+                        switch (GoodNEvil) {
+                            case GoodNEvil > 0:
+                                GoodNEvil--
+                                break;
+                            case GoodNEvil == 0:
+                                break;
+                            case GoodNEvil < 0:
+                                GoodNEvil++
+                                break;
+                        }
+                        console.log(GoodNEvil)
+                        break;
+                    case 'domination':
+                        GoodNEvil--
+                        console.log(GoodNEvil)
+                        break;
+                }
+                $('#q3')
+                    .modal('show')
+            })
+        $('.q3a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'mistakes':
+                        GoodNEvil++
+                        console.log(GoodNEvil)
+                        break;
+                    case 'past':
+                        switch (GoodNEvil) {
+                            case GoodNEvil > 0:
+                                GoodNEvil--
+                                break;
+                            case GoodNEvil == 0:
+                                break;
+                            case GoodNEvil < 0:
+                                GoodNEvil++
+                                break;
+                        }
+                        console.log(GoodNEvil)
+                        break;
+                    case 'choices':
+                        GoodNEvil--
+                        console.log(GoodNEvil)
+                        break;
+                }
+                $('#q4')
+                    .modal('show')
+            })
+        $('.q4a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'family':
+                        LawNChaos++
+                        console.log(LawNChaos)
+                        break;
+                    case 'myself':
+                        switch (LawNChaos) {
+                            case LawNChaos > 0:
+                                LawNChaos--
+                                break;
+                            case LawNChaos == 0:
+                                break;
+                            case LawNChaos < 0:
+                                LawNChaos++
+                                break;
+                        }
+                        console.log(LawNChaos)
+                        break;
+                    case 'noone':
+                        LawNChaos--
+                        console.log(LawNChaos)
+                        break;
+                }
+                $('#q5')
+                    .modal('show')
+            })
+        $('.q5a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'freeThem':
+                        LawNChaos++
+                        console.log(LawNChaos)
+                        break;
+                    case 'feedThem':
+                        switch (LawNChaos) {
+                            case LawNChaos > 0:
+                                LawNChaos--
+                                break;
+                            case LawNChaos == 0:
+                                break;
+                            case LawNChaos < 0:
+                                LawNChaos++
+                                break;
+                        }
+                        console.log(LawNChaos)
+                        break;
+                    case 'leaveThem':
+                        LawNChaos--
+                        console.log(LawNChaos)
+                        break;
+                }
+                $('#q6')
+                    .modal('show')
+            })
+        $('.q6a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'guard':
+                        GoodNEvil++
+                        console.log(GoodNEvil)
+                        break;
+                    case 'ally':
+                        switch (GoodNEvil) {
+                            case GoodNEvil > 0:
+                                GoodNEvil--
+                                break;
+                            case GoodNEvil == 0:
+                                break;
+                            case GoodNEvil < 0:
+                                GoodNEvil++
+                                break;
+                        }
+                        console.log(GoodNEvil)
+                        break;
+                    case 'lord':
+                        GoodNEvil--
+                        console.log(GoodNEvil)
+                        break;
+                }
+                $('#q7')
+                    .modal('show')
+            })
+        $('.q7a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'balance':
+                        LawNChaos++
+                        console.log(LawNChaos)
+                        break;
+                    case 'solitude':
+                        switch (LawNChaos) {
+                            case LawNChaos > 0:
+                                LawNChaos--
+                                break;
+                            case LawNChaos == 0:
+                                break;
+                            case LawNChaos < 0:
+                                LawNChaos++
+                                break;
+                        }
+                        console.log(LawNChaos)
+                        break;
+                    case 'dont':
+                        LawNChaos--
+                        console.log(LawNChaos)
+                        break;
+                }
+                $('#q8')
+                    .modal('show')
+            })
+        $('.q8a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'family':
+                        GoodNEvil++
+                        console.log(GoodNEvil)
+                        break;
+                    case 'knowledge':
+                        switch (GoodNEvil) {
+                            case GoodNEvil > 0:
+                                GoodNEvil--
+                                break;
+                            case GoodNEvil == 0:
+                                break;
+                            case GoodNEvil < 0:
+                                GoodNEvil++
+                                break;
+                        }
+                        console.log(GoodNEvil)
+                        break;
+                    case 'money':
+                        GoodNEvil--
+                        console.log(GoodNEvil)
+                        break;
+                }
+                $('#q9')
+                    .modal('show')
+            })
+        $('.q9a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'gold':
+                        GoodNEvil++
+                        console.log(GoodNEvil)
+                        break;
+                    case 'salt':
+                        switch (GoodNEvil) {
+                            case GoodNEvil > 0:
+                                GoodNEvil--
+                                break;
+                            case GoodNEvil == 0:
+                                break;
+                            case GoodNEvil < 0:
+                                GoodNEvil++
+                                break;
+                        }
+                        console.log(GoodNEvil)
+                        break;
+                    case 'blood':
+                        GoodNEvil--
+                        console.log(GoodNEvil)
+                        break;
+                }
+                $('#q10')
+                    .modal('show')
+            })
+        $('.q10a')
+            .on('click', function () {
+                var dataVal = $(this).data('val')
+                switch (dataVal) {
+                    case 'life':
+                        LawNChaos++
+                        console.log(LawNChaos)
+                        break;
+                    case 'energy':
+                        switch (LawNChaos) {
+                            case LawNChaos > 0:
+                                LawNChaos--
+                                break;
+                            case LawNChaos == 0:
+                                break;
+                            case LawNChaos < 0:
+                                LawNChaos++
+                                break;
+                        }
+                        console.log(LawNChaos)
+                        break;
+                    case 'power':
+                        LawNChaos--
+                        console.log(LawNChaos)
+                        break;
+                }
+                var affinity1 = ''
+                switch (LawNChaos) {
+                    case LawNChaos > 1:
+                        affinity1 = 'Lawful'
+                        break;
+                    case -1 <= LawNChaos <= 1:
+                        affinity1 = 'Neutral'
+                        break;
+                    case LawNChaos < -1:
+                        affinity1 = 'Chaotic'
+                        break;
+                }
+                var affinity2 = ''
+                switch (GoodNEvil) {
+                    case GoodNEvil > 1:
+                        affinity2 = 'Good'
+                        break;
+                    case -1 <= GoodNEvil <= 1:
+                        affinity2 = 'Neutral'
+                        break;
+                    case GoodNEvil < -1:
+                        affinity2 = 'Evil'
+                        break;
+                }
+                affinity = affinity1 + ' ' + affinity2
+                console.log(affinity1, affinity2)
+            })
+
+
+    }
     function idlemana() {
         clearInterval(window.manaInterval)
-        var manaRate = rituals*1.5
+        var manaRate = rituals * 1.5
         window.manaInterval = setInterval(function () {
             if (mana < manaStorage) {
                 mana += manaRate
@@ -324,7 +721,7 @@ $(function () {
     }
     function idleRitual() {
         clearInterval(window.rituInterval)
-        var rituRate = familiars*.5
+        var rituRate = familiars * .5
         window.rituInterval = setInterval(function () {
             if (rituals < ritualStorage) {
                 rituals += rituRate
@@ -336,7 +733,7 @@ $(function () {
             }
             idlemana()
         }, 1000)
-        
+
     }
 
 
